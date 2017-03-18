34d174265eede6da1e98b131b2f24339e0f1f7ea
Piyush Goyal <coderpiyush443@gmail.com>
2017-03-18 23:24:23 +0530

    created login module

---
+++ controllers/login.py
@@ -0,0 +1,24 @@
+from modules import *
+
+import jwt
+
+secret = "bb9f8f5742f313ab5e6b3d93f96f36ab59955d86b71b4a7c"
+
+class LoginHandler(RequestHandler):
+
+	def post(email, pswd):
+
+		password = hashingPassword(pswd)
+		password = hashlib.sha256(password).hexdigest()
+
+		ret = yield db.users.find_one({"email" : email, "pswd" = password})
+
+		if bool(ret):
+
+			token = jwt.encode({"email" : email}, secret, algorithm = 'HS256')
+
+			return {"token" : token, "code" : 200, "status" : "successfull"}
+
+		else:
+
+			return {"code" : 400, "status" : "invalid_credentials"}
