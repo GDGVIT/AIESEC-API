5c87c02f862c4579827042f817ebd7b78f3d79cb
Piyush Goyal <coderpiyush443@gmail.com>
2017-03-20 00:55:23 +0530

    added emails.json

--- server.py
+++ server.py
@@ -1,17 +1,22 @@
+import routes
+from controllers.modules import *
+
 define("port",
   default = 8000,
   help = "Contact the One who made it Contact PIYUSH :P",
   type = int)
-
-secret = "bb9f8f5742f313ab5e6b3d93f96f36ab59955d86b71b4a7c"
-
-db = MotorClient().aiesec
 
 #overloading the Application
 class Application(Application):
 
 	def __init__(self):
 
-		handlers = []
+		handlers = routes.routes
 		settings = dict(debug = True)
 		Application.__init__(self, handlers, **settings)
+
+if __name__ == '__main__':
+    options.parse_command_line()
+    http_server = HTTPServer(Application())
+    http_server.listen(options.port)
+    IOLoop.instance().start()
