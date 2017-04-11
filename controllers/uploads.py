0a2649fe1c26577f3fb033b48c2444147874266f
Piyush Goyal <piyushgoyal443@gmail.com>
2017-04-11 13:01:47 +0530

    updated uploads.py

--- controllers/uploads.py
+++ controllers/uploads.py
@@ -8,19 +8,20 @@ class UploadsHandler(RequestHandler):
 	def post(self):
 
 		token = self.get_argument("token")
-		files = self.request.files
+		files = self.request.files["file"]
 
 		tk = yield db.token.find_one({"token" : token})
-
+		fl_list = []
 		if tk:
 			for fl in files:
-				extn = os.path.splitext(fl)[1]
+				extn = os.path.splitext(fl['filename'])[1]
 				cname = str(uuid.uuid4()) + extn
-				fh = open(__UPLOADS__ + cname, 'wb')
+				fh = open(__UPLOADS__ + cname, 'w')
 				fh.write(fl['body'])
 				fh.close()
-				db.users.update({"email" : tk["email"]},
-					{"$push" : {"files" : __UPLOADS__ + cname}})
+				fl_list.append(__UPLOADS__ + cname)
+			db.users.update({"email" : tk["email"]},
+				{"$push" : {"files" :fl_list}})
 
 			self.write({"code" : 202, "status" : "successfully_uploaded"})
 
