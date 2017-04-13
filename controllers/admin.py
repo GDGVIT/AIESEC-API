ed64b53c904cabd05d966b42dd539036dc74d366
Piyush Goyal <piyushgoyal443@gmail.com>
2017-04-13 14:38:37 +0530

    removed postmessages.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -8,6 +8,7 @@ class AddUserHandler(RequestHandler):
         token = self.get_argument("token")
         aemail = self.get_argument("aemail")
         uemail = self.get_argument("uemail")
+        body = self.get_argument("body")
         tk = yield db.token.find_one({"token" : token})
 
         if tk:
@@ -20,7 +21,8 @@ class AddUserHandler(RequestHandler):
                     self.write({"code" : 405, "status" : "already _a_member"})
 
                 else:
-                    db.users.insert({"email" : uemail})
+                    yield db.users.insert({"email" : uemail})
+                    yield db.bodies.insert({"body" : body, "name" : "", "email" : uemail})
                     self.write({"code" : 200, "status" : "successfull"})
 
             else:
@@ -51,30 +53,6 @@ class RemoveUserHandler(RequestHandler):
 
                 else:
                     self.write({"code" : 400, "status" : "not_a_member"})
-
-            else:
-                self.write({"code" : 300, "status" : "Not_a_admin"})
-
-        else:
-            self.write({"code" : 300, "status" : "Invalid_token"})
-
-class PostMessageHandler(RequestHandler):
-
-    @coroutine
-    def post(self):
-
-        token = self.get_argument("token")
-        email = self.get_argument("email")
-        msg = self.get_argument("message")
-
-        tk = yield db.token.find_one({"token" : token})
-
-        if tk:
-            chk_data = yield db.bodies.find_one({"body" : "eb", "email": aemail})
-
-            if chk_data and (aemail == tk["email"]):
-
-                yield db.messages.insert({"email" : email, "msg" : msg})
 
             else:
                 self.write({"code" : 300, "status" : "Not_a_admin"})
