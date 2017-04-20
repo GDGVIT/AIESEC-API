8df5900e7d8a7bd7d8dc1747d62cfa2a3e2c5f00
Piyush Goyal <piyushgoyal443@gmail.com>
2017-04-21 04:01:01 +0530

    added setToken function

--- controllers/utilities.py
+++ controllers/utilities.py
@@ -1,8 +1,7 @@
 from modules import *
 
-
 def hashingPassword(password, salt = uuid.uuid4().hex):
-    
+
     """
     function to hash user password and return the hashed password
 
@@ -13,3 +12,23 @@ def hashingPassword(password, salt = uuid.uuid4().hex):
 
     np = salt + password
     return pbkdf2_sha256.hash(np), salt
+
+def setToken(email, name):
+
+    """
+    function to set user token and return token
+
+    @params
+    email    :   user email
+    name     :   user name
+    """
+
+    now = datetime.now()
+    time = now.strftime("%d-%m-%Y %I:%M %p")
+
+    token = jwt.encode({"email" : email, "time" : time},
+                        secret, algorithm = 'HS256')
+    yield db.token.insert({"token" : token, "name" : name,
+                    "email" : email})
+
+    return token
