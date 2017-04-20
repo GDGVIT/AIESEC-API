22f8205554785bd7c99662a98d3a1ff0fb91b342
Piyush Goyal <piyushgoyal443@gmail.com>
2017-04-21 02:22:21 +0530

    added adduser.py

---
+++ controllers/adduser.py
@@ -0,0 +1,42 @@
+from modules import *
+
+class AddUserHandler(RequestHandler):
+
+	"""
+	class resposible to handle the add user functions
+
+	route : /adduser
+	parameter : token, amail (admin_email), uemail (user_email), body (working body)
+	"""
+
+    @coroutine
+    def post(self):
+
+        token = self.get_argument("token")
+        aemail = self.get_argument("aemail")
+        uemail = self.get_argument("uemail")
+        body = self.get_argument("body")
+        
+        tk = yield db.token.find_one({"token" : token})
+
+        if tk:
+            chk_data = yield db.bodies.find_one({"body" : "eb", "email": aemail})
+
+            if chk_data and (aemail == tk["email"]):
+
+                isUser = yield db.users.find_one({"email" : uemail})
+
+                if isUser:
+                    self.write({"code" : 402, "msg" : "already_a_member"})
+
+                else:
+                    yield db.users.insert({"email" : uemail})
+                    yield db.bodies.insert({"body" : body, "name" : "",
+                                    "email" : uemail})
+                    self.write({"code" : 200, "msg" : "successful"})
+
+            else:
+                self.write({"code" :101, "msg" : "Not_a_admin"})
+
+        else:
+            self.write({"code" : 100, "msg" : "Invalid_token"})
